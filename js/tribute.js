86f0ada83bc63d5661dc0f51891199ace5f32318
timothy <tim.longoria@gmail.com>
2016-03-12 15:45:37 -0600

    Added quotes, subreddit links, and portfolio footer

--- js/tribute.js
+++ js/tribute.js
@@ -67,6 +67,19 @@ $(document).ready(function() {
     currentPhoto = 0;
     currentDog = null;
   });
+
+  // mini portfolio navigation
+  $('#ab').click(function(){
+    $('#about').toggle(600);
+  });
+ 
+  $('#pr').click(function(){
+    $('#projects').toggle(600);
+  });
+
+  $('#ti').click(function(){
+    $('#timpic').toggle(600);
+  });
 });
 
 // builds thumbnails into gallery to be toggled into
